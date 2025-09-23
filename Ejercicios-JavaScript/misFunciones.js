2ab80fdc7d41dfff0fd584778078a802b85a94db
AguAlbizu <augustoalbizu2302@gmail.com>
2025-09-23 20:24:55 -0300

    avanzo con ejercicios

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -5,3 +5,39 @@
  * @param Parámetro B
  * @return Valor que retorna
  */
+
+function convertirUnidades(unidad, Valor) {
+  let metro, pie, pulgada, yarda;
+
+  console.log(Valor);
+  console.log(isNaN(Valor));
+
+  if (isNaN(Valor)) {
+    alert("El valor ingresado no es correcto");
+    metro = "";
+    pie = "";
+    pulgada = "";
+    yarda = "";
+  } else {
+    if (unidad == "unid_metro") {
+      metro = Valor;
+      pie = 3.28 * metro;
+      pulgada = 39.37 * metro;
+      yarda = 1.0936 * metro;
+    } else if (unidad == "unid_pie") {
+      pie = Valor;
+      metro = 0.3048 * pie;
+      pulgada = 12 * pie;
+      yarda = 0.3333 * pie;
+    } else if (unidad == "unid_pulgada") {
+      // hacer conversion
+    } else if (unidad == "unid_yarda") {
+      // hacer conversion
+      // hacer commit
+    }
+  }
+  document.getElementById("metro").value = metro;
+  document.getElementById("pie").value = pie;
+  document.getElementById("pulgada").value = pulgada;
+  document.getElementById("yarda").value = yarda;
+}
