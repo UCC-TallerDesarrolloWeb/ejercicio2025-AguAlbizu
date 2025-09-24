e9a437318b248e9f99b011e46f644b702f911a68
AguAlbizu <augustoalbizu2302@gmail.com>
2025-09-24 16:47:34 -0300

    operaciones matematicas ejercicio

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -77,3 +77,35 @@ function mostrar_ocultar(valorMO) {
     document.getElementById("divMO").style.display = "none";
   }
 }
+
+function calcularsuma() {
+  let n1, n2;
+
+  n1 = Number(document.getElementsByName("sum_num1")[0].value);
+  n2 = Number(document.getElementsByName("sum_num2")[0].value);
+  document.getElementsByName("sum_total")[0].value = n1 + n2;
+}
+
+function calcularresta() {
+  let n1, n2;
+
+  n1 = Number(document.getElementsByName("res_num1")[0].value);
+  n2 = Number(document.getElementsByName("res_num2")[0].value);
+  document.getElementsByName("res_total")[0].value = n1 - n2;
+}
+
+function calcularmultiplicacion() {
+  let n1, n2;
+
+  n1 = Number(document.getElementsByName("mul_num1")[0].value);
+  n2 = Number(document.getElementsByName("mul_num2")[0].value);
+  document.getElementsByName("mul_total")[0].value = n1 * n2;
+}
+
+function calculardivision() {
+  let n1, n2;
+
+  n1 = Number(document.getElementsByName("div_num1")[0].value);
+  n2 = Number(document.getElementsByName("div_num2")[0].value);
+  document.getElementsByName("div_total")[0].value = n1 / n2;
+}
