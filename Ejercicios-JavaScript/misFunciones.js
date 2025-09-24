9752db01e4326ab6a4b916ba0b57f17c92849f56
AguAlbizu <augustoalbizu2302@gmail.com>
2025-09-24 00:22:34 -0300

    conversion de unidades terminado

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -6,7 +6,7 @@
  * @return Valor que retorna
  */
 
-function convertirUnidades(unidad, Valor) {
+function convertirUnidades(name, Valor) {
   let metro, pie, pulgada, yarda;
 
   console.log(Valor);
@@ -19,21 +19,26 @@ function convertirUnidades(unidad, Valor) {
     pulgada = "";
     yarda = "";
   } else {
-    if (unidad == "unid_metro") {
+    if (name == "unid_metro") {
       metro = Valor;
       pie = 3.28 * metro;
       pulgada = 39.37 * metro;
       yarda = 1.0936 * metro;
-    } else if (unidad == "unid_pie") {
+    } else if (name == "unid_pulgada") {
+      pulgada = Valor;
+      metro = 0.0254 * pulgada;
+      pie = 0.083 * pulgada;
+      yarda = 0.0277 * pulgada;
+    } else if (name == "unid_pie") {
       pie = Valor;
       metro = 0.3048 * pie;
       pulgada = 12 * pie;
       yarda = 0.3333 * pie;
-    } else if (unidad == "unid_pulgada") {
-      // hacer conversion
-    } else if (unidad == "unid_yarda") {
-      // hacer conversion
-      // hacer commit
+    } else if (name == "unid_yarda") {
+      yarda = Valor;
+      metro = 0.914 * yarda;
+      pie = 3 * yarda;
+      pulgada = 36 * yarda;
     }
   }
   document.getElementById("metro").value = metro;
