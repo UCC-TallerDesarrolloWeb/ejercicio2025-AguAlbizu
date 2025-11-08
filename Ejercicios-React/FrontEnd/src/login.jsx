5aa5f1b842bcff4dda6f9a2b5da8ec49e4b7005a
AguAlbizu <augustoalbizu2302@gmail.com>
2025-11-08 08:38:38 -0300

    react, vite, ejercicio login

---
+++ Ejercicios-React/FrontEnd/src/login.jsx
@@ -0,0 +1,46 @@
+import "./Login.css"
+import React, { useState } from "react";
+
+
+const Login = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handlelogin = async (e) => {
+        e.preventDefault();
+        if(username=="admin" && password=="123"){
+            console.log("login ok")
+        }else{
+            console.log("login incorrecto")
+        }
+    }
+
+    return (
+        <div className="login-container">
+            <form className="login-form" onSubmit={handlelogin}>
+                <h2>
+                    Iniciar Sesion
+                </h2>
+                <input
+                type="text"
+                placeholder="Usuario"
+                onChange={(e) => setUsername(e.target.value)}
+                value={username}
+                required
+                />
+                <input
+                type="password"
+                placeholder="Contraseña"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                required
+                />
+                <button type="submit">
+                    Ingresar
+                </button>
+            </form>
+        </div>
+    );
+}
+
+export default Login;
