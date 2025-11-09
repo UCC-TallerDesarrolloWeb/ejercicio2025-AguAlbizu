7110baebe870040cf543b2e02409684f788738bb
AguAlbizu <augustoalbizu2302@gmail.com>
2025-11-09 01:17:36 -0300

    react imagenes

--- Ejercicios-React/FrontEnd/src/Pages/Home.jsx
+++ Ejercicios-React/FrontEnd/src/Pages/Home.jsx
@@ -1,6 +1,27 @@
+import "@styles/home.css"
+import yogaImg from "../assets/HOME/yoga.webp";
+import zumbaImg from "../assets/HOME/zumba.jpg";
+
 const Home = () => {
     return (
-        <h2>Informacion del gym</h2>
+        <div className="card-home">
+            <div>
+                <img src="../../public/home/funcional.jpg" alt="Funcional" />
+                <h2>Funcional</h2>
+            </div>
+            <div>
+                <img src="../../public/home/taekwondo.jpg" alt="Taekwondo" />
+                <h2>Taekwondo</h2>
+            </div>
+            <div>
+                <img src={yogaImg} alt="Yoga" />
+                <h2>Yoga</h2>
+            </div>
+            <div>
+                <img src={zumbaImg} alt="Zumba" />
+                <h2>Zumba</h2>
+            </div>
+        </div>
     )
 }
 
